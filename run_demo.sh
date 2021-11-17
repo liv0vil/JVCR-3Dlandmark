b7b56ec0641b78abd17c1e0709316e3067f6cb5e
Juyeong Hong <59993108+liv0vil@users.noreply.github.com>
2021-11-17 11:12:11 +0900

    Delete run_demo.sh

--- run_demo.sh
+++
@@ -1,7 +0,0 @@
-rm output/output.json
-
-python run_demo.py \
-    --gpus 7 \
-    --resume_p2v2c /home/jyhong/workspace/Jyhong_JVCR-3Dlandmark/checkpoint/model_p2v2c_300wLP.pth.tar \
-    --imgDir ./data/aflw2000_img_paths.json \
-    --outDir ./output/
